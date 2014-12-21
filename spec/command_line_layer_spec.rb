5bcfa215c6e563001dec3ed30506aacdb82a8425
Laurent B <lbnetid+gh@gmail.com>
2014-12-21 15:34:59 +0100

    Basic command line options checked

--- spec/command_line_layer_spec.rb
+++ spec/command_line_layer_spec.rb
@@ -8,4 +8,21 @@ describe StackedConfig::Layers::CommandLineLayer do
     expect(subject.length > 0).to be_truthy
   end
 
+  it 'should have a "help" command line options defined' do
+    expect(subject.keys.include? :help).to be_truthy
+  end
+
+  it 'should have a "auto" command line options defined' do
+    expect(subject.keys.include? :auto).to be_truthy
+  end
+
+  it 'should have a "simulate" command line options defined' do
+    expect(subject.keys.include? :simulate).to be_truthy
+  end
+
+  it 'should have a "verbose" command line options defined' do
+    expect(subject.keys.include? :verbose).to be_truthy
+  end
+
+
 end
