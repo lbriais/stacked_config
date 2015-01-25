1323c6039593d6dd1c4374b94a7331a9f5051609
Laurent B <lbnetid+gh@gmail.com>
2015-01-25 15:19:45 +0100

    Minor

--- lib/stacked_config/layers/executable_gem_layer.rb
+++ lib/stacked_config/layers/executable_gem_layer.rb
@@ -4,12 +4,12 @@ module StackedConfig
     class ExecutableGemLayer < StackedConfig::Layers::GenericLayer
       def possible_sources
         [
-            ['##GEM_CONFIG_ROOT##', 'etc', '##PROGRAM_NAME##.##EXTENSION##' ],
-            ['##GEM_CONFIG_ROOT##', 'etc', '##PROGRAM_NAME##', 'config.##EXTENSION##' ],
-            ['##GEM_CONFIG_ROOT##', 'etc', '##PROGRAM_NAME##', '##PROGRAM_NAME##.##EXTENSION##' ],
-            ['##GEM_CONFIG_ROOT##', 'config', '##PROGRAM_NAME##.##EXTENSION##' ],
-            ['##GEM_CONFIG_ROOT##', 'config', '##PROGRAM_NAME##', 'config.##EXTENSION##' ],
-            ['##GEM_CONFIG_ROOT##', 'config', '##PROGRAM_NAME##', '##PROGRAM_NAME##.##EXTENSION##' ]
+            %w(##GEM_CONFIG_ROOT## etc ##PROGRAM_NAME##.##EXTENSION##),
+            %w(##GEM_CONFIG_ROOT## etc ##PROGRAM_NAME## config.##EXTENSION##),
+            %w(##GEM_CONFIG_ROOT## etc ##PROGRAM_NAME## ##PROGRAM_NAME##.##EXTENSION##),
+            %w(##GEM_CONFIG_ROOT## config ##PROGRAM_NAME##.##EXTENSION##),
+            %w(##GEM_CONFIG_ROOT## config ##PROGRAM_NAME## config.##EXTENSION##),
+            %w(##GEM_CONFIG_ROOT## config ##PROGRAM_NAME## ##PROGRAM_NAME##.##EXTENSION##)
         ]
       end
 
