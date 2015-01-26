0284703696f193435185895b01e49ecd6b8b0d6a
Laurent B <lbnetid+gh@gmail.com>
2015-01-26 21:36:11 +0100

    Generic method to display potential config files
    Signed-off-by: Laurent B <lbnetid+gh@gmail.com>

--- lib/stacked_config/layers/generic_layer.rb
+++ lib/stacked_config/layers/generic_layer.rb
@@ -13,7 +13,38 @@ module StackedConfig
         rescan
       end
 
+      def possible_config_files(places=possible_sources)
+        result = {}
+        already_found = false
+        browse_files(places) do |file|
+          exists = File.readable?(file)
+          used = false
+          already_found = used = true if exists and not already_found
+          result[file] = {
+              exists: exists,
+              used: used
+          }
+        end
+        result
+      end
+
       private
+
+
+      def browse_files(places, &block)
+        places.each do |path_array|
+          begin
+            potential_config_file = File.join(path_array.map { |path_part| perform_substitutions path_part })
+          rescue
+            #do nothing
+          end
+          return unless potential_config_file
+          EXTENSIONS.each do |extension|
+            file  = potential_config_file.gsub '##EXTENSION##', extension
+            yield file
+          end
+        end
+      end
 
       def set_config_file(places)
         @file_name = nil
