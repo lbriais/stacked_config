3c6991ead679f24d6a124ad8c65ad01abeac3626
Laurent B <lbnetid+gh@gmail.com>
2015-02-01 18:28:01 +0100

    Minor fix for sub-gem spec

--- test/tstgem/spec/global_spec.rb
+++ test/tstgem/spec/global_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 describe 'Using stacked_config within a gem' do
 
 	it 'should detect we are in a gem' do
-		expect(StackedConfig::SourceHelper.executable_gem_config_root).to_not be_nil
-		expect(StackedConfig::SourceHelper.executable_gem_config_root =~ /rspec-core/).to be_truthy
+		expect(StackedConfig::Layers::ExecutableGemLayer.executable_gem_config_root).to_not be_nil
+		expect(StackedConfig::Layers::ExecutableGemLayer.executable_gem_config_root =~ /rspec-core/).to be_truthy
 	end
 
 end
